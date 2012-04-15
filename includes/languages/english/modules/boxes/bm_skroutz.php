8bd8596e43343b91203ec96dff141b8af5c9861a
Fotos Georgiadis <fotos@skroutz.gr>
2012-04-15 20:26:57 +0300

    Added greek language and updated texts

--- includes/languages/english/modules/boxes/bm_skroutz.php
+++ includes/languages/english/modules/boxes/bm_skroutz.php
@@ -1,8 +1,7 @@
 <?php // vim: set ft=php et ts=4 sts=4 sw=4 ai si:
 
-define('MODULE_BOXES_SKROUTZ_TITLE', 'Skroutz Login');
-define('MODULE_BOXES_SKROUTZ_DESCRIPTION', 'Skroutz Login Button');
-define('MODULE_BOXES_SKROUTZ_BOX_TITLE', 'Skroutz Login');
-define('MODULE_BOXES_SKROUTZ_BUTTON', 'Skroutz Login');
+define('MODULE_BOXES_SKROUTZ_TITLE', 'Skroutz Easy');
+define('MODULE_BOXES_SKROUTZ_DESCRIPTION', 'Login with Skroutz Easy');
+define('MODULE_BOXES_SKROUTZ_BUTTON', 'Login with Skroutz Easy');
 
 ?>
---
+++ includes/languages/greek/modules/boxes/bm_skroutz.php
@@ -0,0 +1,7 @@
+<?php // vim: set ft=php et ts=4 sts=4 sw=4 ai si:
+
+define('MODULE_BOXES_SKROUTZ_TITLE', 'Skroutz Easy');
+define('MODULE_BOXES_SKROUTZ_DESCRIPTION', 'Σύνδεση με Skroutz Easy');
+define('MODULE_BOXES_SKROUTZ_BUTTON', 'Σύνδεση με Skroutz Easy');
+
+?>
