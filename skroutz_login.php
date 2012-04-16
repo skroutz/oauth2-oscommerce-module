be73c38a8144d3b00ef8dc41bacceb4534a1cd85
Fotos Georgiadis <fotos@skroutz.gr>
2012-04-16 20:00:47 +0300

    Formatted the code after the introduction of an extra if statement

--- skroutz_login.php
+++ skroutz_login.php
@@ -13,52 +13,52 @@ if ($_POST['mytype'] == 1){
 
     $error = false;
     if (isset($HTTP_POST_VARS['action']) && ($HTTP_POST_VARS['action'] == 'process') && isset($HTTP_POST_VARS['formid']) && ($HTTP_POST_VARS['formid'] == $sessiontoken)) {
-    $email_address = tep_db_prepare_input($HTTP_POST_VARS['email_address']);
+        $email_address = tep_db_prepare_input($HTTP_POST_VARS['email_address']);
 
-    // Check if email exists
-    $check_customer_query = tep_db_query("select customers_id, customers_firstname, customers_password, customers_email_address, customers_default_address_id from " . TABLE_CUSTOMERS . " where customers_email_address = '" . tep_db_input($email_address) . "'");
+        // Check if email exists
+        $check_customer_query = tep_db_query("select customers_id, customers_firstname, customers_password, customers_email_address, customers_default_address_id from " . TABLE_CUSTOMERS . " where customers_email_address = '" . tep_db_input($email_address) . "'");
 
-    if (!tep_db_num_rows($check_customer_query)) {
-        $error = true;
-    } else if (!tep_session_is_registered('oauth_login')) {
-        $error = true;
-    } else {
-        // unregister the session variable
-        tep_session_unregister('oauth_login');
-
-        $check_customer = tep_db_fetch_array($check_customer_query);
-
-        if (SESSION_RECREATE == 'True') {
-            tep_session_recreate();
-        }
-
-        $check_country_query = tep_db_query("select entry_country_id, entry_zone_id from " . TABLE_ADDRESS_BOOK . " where customers_id = '" . (int)$check_customer['customers_id'] . "' and address_book_id = '" . (int)$check_customer['customers_default_address_id'] . "'");
-        $check_country = tep_db_fetch_array($check_country_query);
-
-        $customer_id = $check_customer['customers_id'];
-        $customer_default_address_id = $check_customer['customers_default_address_id'];
-        $customer_first_name = $check_customer['customers_firstname'];
-        $customer_country_id = $check_country['entry_country_id'];
-        $customer_zone_id = $check_country['entry_zone_id'];
-        tep_session_register('customer_id');
-        tep_session_register('customer_default_address_id');
-        tep_session_register('customer_first_name');
-        tep_session_register('customer_country_id');
-        tep_session_register('customer_zone_id');
-
-        tep_db_query("update " . TABLE_CUSTOMERS_INFO . " set customers_info_date_of_last_logon = now(), customers_info_number_of_logons = customers_info_number_of_logons+1 where customers_info_id = '" . (int)$customer_id . "'");
-
-        // reset session token
-        $sessiontoken = md5(tep_rand() . tep_rand() . tep_rand() . tep_rand());
-
-        if (sizeof($navigation->snapshot) > 0) {
-            $origin_href = tep_href_link($navigation->snapshot['page'], tep_array_to_string($navigation->snapshot['get'], array(tep_session_name())), $navigation->snapshot['mode']);
-            $navigation->clear_snapshot();
-            tep_redirect($origin_href);
+        if (!tep_db_num_rows($check_customer_query)) {
+            $error = true;
+        } else if (!tep_session_is_registered('oauth_login')) {
+            $error = true;
         } else {
-            tep_redirect(tep_href_link(FILENAME_DEFAULT));
+            // unregister the session variable
+            tep_session_unregister('oauth_login');
+
+            $check_customer = tep_db_fetch_array($check_customer_query);
+
+            if (SESSION_RECREATE == 'True') {
+                tep_session_recreate();
+            }
+
+            $check_country_query = tep_db_query("select entry_country_id, entry_zone_id from " . TABLE_ADDRESS_BOOK . " where customers_id = '" . (int)$check_customer['customers_id'] . "' and address_book_id = '" . (int)$check_customer['customers_default_address_id'] . "'");
+            $check_country = tep_db_fetch_array($check_country_query);
+
+            $customer_id = $check_customer['customers_id'];
+            $customer_default_address_id = $check_customer['customers_default_address_id'];
+            $customer_first_name = $check_customer['customers_firstname'];
+            $customer_country_id = $check_country['entry_country_id'];
+            $customer_zone_id = $check_country['entry_zone_id'];
+            tep_session_register('customer_id');
+            tep_session_register('customer_default_address_id');
+            tep_session_register('customer_first_name');
+            tep_session_register('customer_country_id');
+            tep_session_register('customer_zone_id');
+
+            tep_db_query("update " . TABLE_CUSTOMERS_INFO . " set customers_info_date_of_last_logon = now(), customers_info_number_of_logons = customers_info_number_of_logons+1 where customers_info_id = '" . (int)$customer_id . "'");
+
+            // reset session token
+            $sessiontoken = md5(tep_rand() . tep_rand() . tep_rand() . tep_rand());
+
+            if (sizeof($navigation->snapshot) > 0) {
+                $origin_href = tep_href_link($navigation->snapshot['page'], tep_array_to_string($navigation->snapshot['get'], array(tep_session_name())), $navigation->snapshot['mode']);
+                $navigation->clear_snapshot();
+                tep_redirect($origin_href);
+            } else {
+                tep_redirect(tep_href_link(FILENAME_DEFAULT));
+            }
         }
-    }
     }
 
     if ($error == true) {
